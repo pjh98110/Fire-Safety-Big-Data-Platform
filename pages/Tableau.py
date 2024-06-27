2b217021e7ce23c1af1c3dd165d627d3d0b64d61
Jonghyeon <128576949+pjh98110@users.noreply.github.com>
2024-06-28 04:24:31 +0900

    Update Tableau.py

--- pages/Tableau.py
+++ pages/Tableau.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.colored_header import colored_header
 
@@ -11,12 +9,6 @@ st.set_page_config(layout="wide")
 if "page" not in st.session_state:
     st.session_state.page = "Home"
 
-DATA_PATH = "./"
-SEED = 42
-
-# 데이터 로드
-data = pd.read_csv(f"{DATA_PATH}test_data.csv")
-
 
 # 타이틀
 colored_header(
@@ -26,8 +18,8 @@ colored_header(
 )
 
 
-
 st.title("2023년 지역별 화재 및 기상데이터 시각화")
+st.write("팝업 차단으로 보이지 않는다면 이 링크로 접속해 주세요. https://public.tableau.com/app/profile/jonghyeon.park/viz/_17194904158790/1_1")
 
 # Tableau Public에서 대시보드 임베딩
 # 여기에 Tableau Public 대시보드 URL을 입력합니다.
